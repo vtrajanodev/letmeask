020e7c547862378bc15d84c0ac131dfadc6433b5
Victor Trajano <trajanodev@hotmail.com>
2021-08-26 14:26:36 -0400

    Authentication finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 import './styles/global.scss'
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 
 function App() {
+
+ 
   return (
     <>
-      <Home />
-    
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
+      </BrowserRouter>
     </>
 
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,29 +1,18 @@
-import { useHistory } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
-
-import '../styles/auth.scss'
-import { useContext } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
 import { useAuth } from '../hooks/useAuth'
 
+import '../styles/auth.scss'
 
-export function Home() {
-    const history = useHistory()
-    const { user, signInWithGoogle } = useAuth()
 
-    async function handleCreateRoom(){
-        if(!user){
-            await signInWithGoogle()
-        }
+export function NewRoom() {
 
-        history.push('/rooms/new')
-    }
-
+    const { user } = useAuth()
 
     return (
         <div id="page-auth">
@@ -35,20 +24,19 @@ export function Home() {
             <main className="main-content">
                 <div>
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room" onClick={handleCreateRoom}>
-                        <img src={googleIconImg} alt="Logo do google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form action="">
                         <input
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar na sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
                 </div>
             </main>
         </div>
